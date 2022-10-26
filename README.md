18f646033fc060dffb80ae66642f04f3e801a200
marko.vukosav <marko.vukosav@rimac-automobili.com>
2022-10-26 16:41:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# AIR2204
+# deep_conference
+
+A mobile app for Deep conferences
+
+## Getting Started
---
+++ android/app/src/main/kotlin/com/deep/conference/deep_conference/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.deep.conference.deep_conference
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
