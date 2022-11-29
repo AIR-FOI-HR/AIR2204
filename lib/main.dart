8bb07cbe75bb3d2aa95a5f4421e78bd2815435bb
Nikola Jug <nikola.jug1@gmail.com>
2022-11-29 16:14:31 +0100

    Fixed comments on pull request

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'app.dart';
 import 'data/firebase_options.dart';
 
 dynamic main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp( 
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
