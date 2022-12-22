0698430f96231683d585ca1166fa3a83b6ad68fc
karlo-rusovan <karlorus@gmail.com>
2022-12-22 12:10:31 +0100

    fixed error in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -8,8 +8,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
-import 'package:deep_conference/application/logic/speaker_cubit.dart';
 import 'package:deep_conference/domain/repositories/speaker_repository.dart';
 import 'application/logic/authentication_cubit.dart';
 import 'application/logic/navigation_cubit.dart';
@@ -42,6 +40,7 @@ class MyApp extends StatelessWidget {
         ),
         RepositoryProvider<UserRepository>(
           create: (context) => UserRepository(auth: auth, firestore: firestore),
+        ),
         RepositoryProvider<SpeakerRepository>(
           create: (context) => SpeakerRepository(firestore: firestore),
         )
